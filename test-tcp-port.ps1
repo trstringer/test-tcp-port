5aa9404edd4ba2f91f02369cfd529c1e38879ee1
Thomas Stringer <sqlsalt@outlook.com>
2015-04-07 09:30:56 -0400

    Initial commit

---
+++ test-tcp-port.ps1
@@ -0,0 +1,50 @@
+function Test-Port {
+    param (
+        [Parameter(Mandatory = $false)]
+        [string]$HostName,
+
+        [Parameter(Mandatory = $false)]
+        [string]$IpAddress,
+
+        [Parameter(Mandatory = $true)]
+        [int]$Port
+    )
+
+    if (!$HostName -and !$IpAddress) {
+        Write-Warning "You need to specify either hostname or IP address!!!"
+        return
+    }
+
+    $IpAddresses = @()
+
+    if ($HostName) {
+        $IpAddressesFromHostName = [System.Net.Dns]::GetHostAddresses($HostName)
+
+        foreach ($IpAddressFromHostName in $IpAddressesFromHostName) {
+            "IP address retrieved :: $($IpAddressFromHostName.ToString())"
+            $IpAddresses += $IpAddressFromHostName
+        }
+    }
+    else {
+        $IpAddresses += [System.Net.IPAddress]::Parse($IpAddress)
+    }
+
+    $TcpClient = New-Object System.Net.Sockets.TcpClient
+
+    foreach ($IpAddressToTest in $IpAddresses) {
+        try {
+            $TcpClient.Connect($IpAddressToTest, $Port)
+            if ($TcpClient.Connected) {
+                "Connection successful to $($IpAddressToTest.ToString()):$Port"
+            }
+            else {
+                "Connection unsuccessful to $($IpAddressToTest.ToString()):$Port"
+            }
+        }
+        catch {
+            "Connection unsuccessful to $($IpAddressToTest.ToString()):$Port"
+        }
+    }
+
+    $TcpClient.Dispose()
+}
